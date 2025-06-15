52b882ce17f5775a75d45ad15d37a007d3dca2af
sr-alex <alexandre.n314159@gmail.com>
2025-06-15 15:32:26 -0300

    Resposta ao desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 32);
+Iphone iphone = new Iphone("123456789", "Nokia 3310", "123456789012345", 32);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Snake");
